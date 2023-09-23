1b1faa3702cd5977b4ee8d088f918f4d05c80137
Leandro <leandro.feruhl@gmail.com>
2023-09-23 20:36:39 -0300

    Implement bubble sort, two crystal balls, queue and stack

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const JUMP_AMOUNT = Math.floor(Math.sqrt(breaks.length));
 
+    let i = JUMP_AMOUNT;
+    for (; i < breaks.length; i += JUMP_AMOUNT) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= JUMP_AMOUNT;
+    for (let j = 0; j <= JUMP_AMOUNT && i < breaks.length; j++, i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
